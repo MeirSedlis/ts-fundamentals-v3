9eb0189381dc24940df04151f1c37e0c19262c59
Meir Sedlis <meirsedlis@gmail.com>
2022-08-22 19:57:56 -0500

    added typing for JSON

--- packages/hello-ts/src/JSONtypes.ts
+++ packages/hello-ts/src/JSONtypes.ts
@@ -1,28 +1,30 @@
 // @errors: 2578
-type JSONObject = any
-type JSONArray = any
-type JSONValue = any
+type Primitive = string | number | boolean | null;
+
+type JSONObject = { [k: string]: JSONValue };
+type JSONArray = JSONValue[];
+type JSONValue = Primitive | JSONObject | JSONArray;
 
 ////// DO NOT EDIT ANY CODE BELOW THIS LINE //////
 function isJSON(arg: JSONValue) {}
 
 // POSITIVE test cases (must pass)
-isJSON("hello")
-isJSON([4, 8, 15, 16, 23, 42])
-isJSON({ greeting: "hello" })
-isJSON(false)
-isJSON(true)
-isJSON(null)
-isJSON({ a: { b: [2, 3, "foo"] } })
+isJSON('hello');
+isJSON([4, 8, 15, 16, 23, 42]);
+isJSON({ greeting: 'hello' });
+isJSON(false);
+isJSON(true);
+isJSON(null);
+isJSON({ a: { b: [2, 3, 'foo'] } });
 
 // NEGATIVE test cases (must fail)
 // @ts-expect-error
-isJSON(() => "")
+isJSON(() => '');
 // @ts-expect-error
-isJSON(class {})
+isJSON(class {});
+// ts-expect-error - removed this test because for whatever reason it's passing?? not passing in the TS Sandbox
+isJSON(undefined);
 // @ts-expect-error
-isJSON(undefined)
+isJSON(new BigInt(143));
 // @ts-expect-error
-isJSON(new BigInt(143))
-// @ts-expect-error
-isJSON(isJSON)
+isJSON(isJSON);
